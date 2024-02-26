d1d5c8c0993b02eb3561b515ff492f590c8fbcda
Ali Eren KÖSE <alierenkose@gmail.com>
2024-02-26 20:21:45 +0300

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Basamak Sayısı Giriniz : ");       // orta yükseklik yıldız sayısı iste
+        int n = input.nextInt();                                // yüksekliği kaydet ve n'e ata
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int i = 1; i <= n; i++) {                    // yükseklikten azaltalarak yukarıdan aşşağı sırala
+            for (int k = 1; k <= (n - i); k++) {         // sol tarafı 1 arttırarak bolşuk yok
+                System.out.print(" ");                     // tekrar kadar boşluk yazdır
+            }
+            for (int x = 1; x <= (2 * i) - 1; x++) {        // sıralı artarak yıldız yazdır
+                System.out.print("*");
+            }
+            System.out.println();                   // her yıldız satırından sonra satır atla
+        }
+
+        for (int a = n ; a > 0 ; a--) {             // elmasın en kalın yerinden azaltarak boşluk ata
+            for (int b = n; b > a-1; b--) {         // n sayısını soldan eksilterek boşluk koy
+                System.out.print(" ");              // tekrar kadar boşluk yazdır.
+            }
+            for (int c = 1 ; c < (a*2-2) ; c++) {       //n sayısını 2  azaltarak yıldız yazdır
+                System.out.print("*");                  // yıldızı yazdır
+            }
+            System.out.println();
         }
     }
-}
+}               //Ali Eren KÖSE 26/02/2024  20:05
